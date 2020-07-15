903736d51b97b1f772116b5419b00438ee6a5f3b
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-15 15:53:58 -0700

    add and view list UI logic
    co-authored-by: Teresa Rosinski <trosinski89@gmail.com>

--- todoList/Program.cs
+++ todoList/Program.cs
@@ -7,7 +7,51 @@ namespace ToDoList
   {
     public static void Main()
     {
+      List<Item> toDoList = new List<Item>() { };
+      Console.WriteLine("Welcome to your ToDo list. Press 1 to add an item. Press 2 to view your list");
+      string response = Console.ReadLine();
 
+      if (response == "1")
+      {
+        Console.WriteLine("Enter an item for your to-do list: "); //take in user item
+        string listItem = Console.ReadLine(); //set user item to a variable
+        Item newItem = new Item(listItem);
+        Main();
+      }
+      else if (response == "2")
+      {
+        //Incorrect way 
+        // List<Item> result = Item.GetAll();
+        // foreach (Item newItem in toDoList)
+        // {
+        //   Console.WriteLine(result.Description);
+        // }
+        //Correct Way
+        List<Item> result = Item.GetAll();
+        foreach (Item thisItem in result)//thisItem represents each actual item in result
+        {
+          Console.WriteLine(thisItem.Description);//the only way to access the description of the item is by touching the actual item not the list that holds all the items
+        }
+
+        //List<Item> newList = new List<Item> { newItem };
+
+        // Console.WriteLine(result);
+      }
+
+      //foreach (Item thisItem in result)
+      //{
+      //Console.WriteLine("Output from empty list GetAll test: " + thisItem.Description);
+
+      // Console.WriteLine("Would you like to add another item? [Y] for yes or [Enter for Main Menu] ");
+      // string addAnother = Console.ReadLine();
+      // if (addAnother == "Y" || "y")
+      // {
+      //   Console.WriteLine("Enter an item for your to-do list: ");
+      // }
+      // else Main();
+      //Console.WriteLine(newItem.Description);//must do .Description to access details
+      //Code for allowing the user to enter multiple items  - continual prompt for entering more items
+      //code to show the entire list of items
     }
   }
 }
