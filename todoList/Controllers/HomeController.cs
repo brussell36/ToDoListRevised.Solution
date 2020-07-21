33759e319f8074e5b910a14a8af3af12e1eb92c9
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-21 10:37:31 -0700

    separate items controllers from home controllers
    Co-authored-by: Zentraxius <clanalia55@gmail.com>
    Co-authored-by: Hannah <hannah.beinstein@gmail.com>

--- todoList/Controllers/HomeController.cs
+++ todoList/Controllers/HomeController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
 
+using Microsoft.AspNetCore.Mvc;
+
 namespace ToDoList.Controllers
 {
   public class HomeController : Controller
@@ -10,21 +12,8 @@ namespace ToDoList.Controllers
     [HttpGet("/")]
     public ActionResult Index()
     {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
-    }
-
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm()
-    {
       return View();
     }
 
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
-    }
   }
 }
