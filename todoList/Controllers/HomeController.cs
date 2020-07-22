482bb3288bb7d380a7abd7eaea3cede9b1d74985
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-07-22 11:07:42 -0700

    add delete route to ItemsController and add deleteAll.cshtml
    Co-authored-by: SPdowns <spdowns@protonmail.com>

--- todoList/Controllers/HomeController.cs
+++ todoList/Controllers/HomeController.cs
@@ -2,8 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
 
-using Microsoft.AspNetCore.Mvc;
-
 namespace ToDoList.Controllers
 {
   public class HomeController : Controller
