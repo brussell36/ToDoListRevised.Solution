891928a572c5a36969f12f977994af11180db516
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-07-23 10:52:37 -0700

    add failing test for assigning id to category
    Co-authored-by: aesadin <aesadin@gmail.com>

--- ToDoList.Tests/ModelTests/CategoryTests.cs
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -14,5 +14,16 @@ namespace ToDoList.Tests
         Category newCategory = new Category("test category");
         Assert.AreEqual(typeof(Category), newCategory.GetType());
       }
+
+      [TestMethod]
+      public void GetId_ReturnsCategoryId_Int()
+      {
+        string name = "Test Category";
+        Category newCategory = new Category(name);
+
+        int result = newCategory.Id;
+
+        Assert.AreEqual(1, result);
+      }
   }
 }
